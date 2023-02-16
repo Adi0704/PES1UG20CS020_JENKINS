6bd6e60b3ffa62373a42aca9c5ee227ba941615f
Tejas Goyal <jaz.goyal@gmail.com>
2023-02-16 14:25:40 +0530

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS020 has successfully built and run\n";
+  return 0;
+}
